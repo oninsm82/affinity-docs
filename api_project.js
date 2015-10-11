e7ca1dca7ac64f306e15255952879aa53175e1b0
unknown <onin@superlever.com>
2015-10-11 15:39:39 +0800

    first upload

---
+++ api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "Affinity Engine",
+  "version": "0.1.0",
+  "description": "Affinity Engine Documentation",
+  "title": "Affinity Engine Docs",
+  "url": "http://affinity-engine.herokuapp.com",
+  "sampleUrl": "http://affinity-engine.herokuapp.com",
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-10-11T01:54:53.766Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
